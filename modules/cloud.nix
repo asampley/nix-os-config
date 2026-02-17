02a2db42c4aa71c76e64f474053c2ce5f6a6cbbf
Alex Sampley <alex.sampley@gmail.com>
2026-02-17 09:16:41 -0700

    Fix matrix settings to only apply when tuwunel is enabled

--- modules/cloud.nix
+++ modules/cloud.nix
@@ -30,111 +30,113 @@
     let
       cfg = config.my.cloud;
     in
-    {
-      services.nextcloud = lib.mkIf cfg.nextcloud.enable {
-        enable = true;
+    lib.mkMerge [
+      (lib.mkIf cfg.nextcloud.enable {
+        services.nextcloud = {
+          enable = true;
 
-        https = cfg.nextcloud.https;
+          https = cfg.nextcloud.https;
 
-        package = pkgs.nextcloud32;
-        hostName = "${cfg.nextcloud.hostName}";
+          package = pkgs.nextcloud32;
+          hostName = "${cfg.nextcloud.hostName}";
 
-        extraAppsEnable = true;
-        extraApps = with config.services.nextcloud.package.packages.apps; {
-          inherit
-            contacts
-            calendar
-            news
-            notes
-            tasks
-            ;
-        };
-
-        config = {
-          adminpassFile = "/etc/nextcloud-admin-pass";
-          dbtype = "pgsql";
-          dbname = "nextcloud";
-          dbuser = "nextcloud";
-          dbhost = "/run/postgresql";
-        };
-
-        settings = {
-          log_type = "systemd";
-        };
-      };
-
-      systemd.services.nextcloud-custom-config = lib.mkIf cfg.nextcloud.enable {
-        path = [
-          config.services.nextcloud.occ
-        ];
-        script = ''
-          nextcloud-occ theming:config url "https://${cfg.nextcloud.hostName}";
-        '';
-        after = [ "nextcloud-setup.service" ];
-        wantedBy = [ "multi-user.target" ];
-      };
-
-      services.nginx.virtualHosts."${cfg.nextcloud.hostName}" = lib.mkIf cfg.nextcloud.enable {
-        enableACME = cfg.nextcloud.https;
-        forceSSL = cfg.nextcloud.https;
-      };
-
-      networking.firewall.allowedTCPPorts = lib.optionals (cfg.nextcloud.enable && cfg.nextcloud.https) [
-        443
-        80
-      ];
-
-      services.postgresql = lib.mkIf cfg.nextcloud.enable {
-        enable = true;
-
-        ensureDatabases = [ "nextcloud" ];
-        ensureUsers = [
-          {
-            name = "nextcloud";
-            ensureDBOwnership = true;
-          }
-        ];
-      };
-
-      systemd.services."nextcloud-setup" = lib.mkIf cfg.nextcloud.enable {
-        requires = [ "postgresql.service" ];
-        after = [ "postgresql.service" ];
-      };
-
-      services.borgbackup.jobs = lib.mkIf (cfg.nextcloud.enable && cfg.nextcloud.borgbackup.enable) {
-        "${cfg.nextcloud.borgbackup.name}" =
-          let
-            cfgnc = config.services.nextcloud;
-          in
-          {
-            paths = [
-              cfgnc.datadir
-              "/tmp/output"
-            ];
-
-            readWritePaths = [ "${cfgnc.datadir}" ];
-
-            privateTmp = true;
-
-            preHook = ''
-              # Make directory for additional outputs
-              ${pkgs.coreutils}/bin/mkdir -m 777 /tmp/output/
-
-              # Lock nextcloud files for consistency
-              ${cfgnc.occ}/bin/nextcloud-occ maintenance:mode --on
-
-              # Backup database while locked
-              ${config.security.sudo.package}/bin/sudo -u nextcloud ${config.services.postgresql.package}/bin/pg_dump -U ${cfgnc.config.dbuser} ${cfgnc.config.dbname} -f /tmp/output/pg_dump.sql
-            '';
-
-            postHook = ''
-              # Unlock nextcloud files
-              ${cfgnc.occ}/bin/nextcloud-occ maintenance:mode --off
-            '';
-
-            startAt = lib.mkDefault "*-*-* *:00:00";
-            persistentTimer = lib.mkDefault true;
+          extraAppsEnable = true;
+          extraApps = with config.services.nextcloud.package.packages.apps; {
+            inherit
+              contacts
+              calendar
+              news
+              notes
+              tasks
+              ;
           };
-      };
-    };
+
+          config = {
+            adminpassFile = "/etc/nextcloud-admin-pass";
+            dbtype = "pgsql";
+            dbname = "nextcloud";
+            dbuser = "nextcloud";
+            dbhost = "/run/postgresql";
+          };
+
+          settings = {
+            log_type = "systemd";
+          };
+        };
+
+        systemd.services.nextcloud-custom-config = {
+          path = [
+            config.services.nextcloud.occ
+          ];
+          script = ''
+            nextcloud-occ theming:config url "https://${cfg.nextcloud.hostName}";
+          '';
+          after = [ "nextcloud-setup.service" ];
+          wantedBy = [ "multi-user.target" ];
+        };
+
+        services.nginx.virtualHosts."${cfg.nextcloud.hostName}" = {
+          enableACME = cfg.nextcloud.https;
+          forceSSL = cfg.nextcloud.https;
+        };
+
+        networking.firewall.allowedTCPPorts = lib.optionals cfg.nextcloud.https [
+          443
+          80
+        ];
+
+        services.postgresql = {
+          enable = true;
+
+          ensureDatabases = [ "nextcloud" ];
+          ensureUsers = [
+            {
+              name = "nextcloud";
+              ensureDBOwnership = true;
+            }
+          ];
+        };
+
+        systemd.services."nextcloud-setup" = {
+          requires = [ "postgresql.service" ];
+          after = [ "postgresql.service" ];
+        };
+
+        services.borgbackup.jobs = lib.mkIf cfg.nextcloud.borgbackup.enable {
+          "${cfg.nextcloud.borgbackup.name}" =
+            let
+              cfgnc = config.services.nextcloud;
+            in
+            {
+              paths = [
+                cfgnc.datadir
+                "/tmp/output"
+              ];
+
+              readWritePaths = [ "${cfgnc.datadir}" ];
+
+              privateTmp = true;
+
+              preHook = ''
+                # Make directory for additional outputs
+                ${pkgs.coreutils}/bin/mkdir -m 777 /tmp/output/
+
+                # Lock nextcloud files for consistency
+                ${cfgnc.occ}/bin/nextcloud-occ maintenance:mode --on
+
+                # Backup database while locked
+                ${config.security.sudo.package}/bin/sudo -u nextcloud ${config.services.postgresql.package}/bin/pg_dump -U ${cfgnc.config.dbuser} ${cfgnc.config.dbname} -f /tmp/output/pg_dump.sql
+              '';
+
+              postHook = ''
+                # Unlock nextcloud files
+                ${cfgnc.occ}/bin/nextcloud-occ maintenance:mode --off
+              '';
+
+              startAt = lib.mkDefault "*-*-* *:00:00";
+              persistentTimer = lib.mkDefault true;
+            };
+        };
+      })
+    ];
 }
--- modules/matrix.nix
+++ modules/matrix.nix
@@ -23,53 +23,55 @@
     let
       cfg = config.my.matrix;
     in
-    {
-      services.matrix-tuwunel = lib.mkIf cfg.tuwunel.enable {
-        enable = true;
-        settings.global = {
-          server_name = "${cfg.tuwunel.publicDomainName}";
-          port = [ 8008 ];
-          allow_federation = false;
-          allow_registration = true;
-          registration_token_file = "/etc/tuwunel/.reg_token";
+    lib.mkMerge [
+      (lib.mkIf cfg.tuwunel.enable {
+        services.matrix-tuwunel = {
+          enable = true;
+          settings.global = {
+            server_name = "${cfg.tuwunel.publicDomainName}";
+            port = [ 8008 ];
+            allow_federation = false;
+            allow_registration = true;
+            registration_token_file = "/etc/tuwunel/.reg_token";
+          };
         };
-      };
 
-      services.nginx.virtualHosts."${cfg.tuwunel.publicDomainName}" =
-        lib.mkIf (cfg.tuwunel.publicDomainName != null)
-          {
-            addSSL = true;
-            enableACME = true;
-            locations = {
-              "= /.well-known/matrix/server" = {
-                extraConfig = ''
-                  add_header Access-Control-Allow-Origin *;
-                  default_type application/json;
-                '';
-                return = "200 '{\"m.server\": \"${cfg.tuwunel.domainName}:443\"}'";
-              };
+        services.nginx.virtualHosts."${cfg.tuwunel.publicDomainName}" =
+          lib.mkIf (cfg.tuwunel.publicDomainName != null)
+            {
+              addSSL = true;
+              enableACME = true;
+              locations = {
+                "= /.well-known/matrix/server" = {
+                  extraConfig = ''
+                    add_header Access-Control-Allow-Origin *;
+                    default_type application/json;
+                  '';
+                  return = "200 '{\"m.server\": \"${cfg.tuwunel.domainName}:443\"}'";
+                };
 
-              "= /.well-known/matrix/client" = {
-                extraConfig = ''
-                  add_header Access-Control-Allow-Origin *;
-                  default_type application/json;
-                '';
-                return = "200 '{\"m.homeserver\": {\"base_url\": \"https://${cfg.tuwunel.domainName}\"}}'";
-              };
-            };
-          };
-
-      services.nginx.virtualHosts."${cfg.tuwunel.domainName}" =
-        lib.mkIf (cfg.tuwunel.domainName != null)
-          {
-            enableACME = true;
-            onlySSL = true;
-            locations = {
-              "/" = {
-                proxyPass = "http://localhost:${builtins.toString (builtins.elemAt config.services.matrix-tuwunel.settings.global.port 0)}";
-                recommendedProxySettings = true;
+                "= /.well-known/matrix/client" = {
+                  extraConfig = ''
+                    add_header Access-Control-Allow-Origin *;
+                    default_type application/json;
+                  '';
+                  return = "200 '{\"m.homeserver\": {\"base_url\": \"https://${cfg.tuwunel.domainName}\"}}'";
+                };
               };
             };
-          };
-    };
+
+        services.nginx.virtualHosts."${cfg.tuwunel.domainName}" =
+          lib.mkIf (cfg.tuwunel.domainName != null)
+            {
+              enableACME = true;
+              onlySSL = true;
+              locations = {
+                "/" = {
+                  proxyPass = "http://localhost:${builtins.toString (builtins.elemAt config.services.matrix-tuwunel.settings.global.port 0)}";
+                  recommendedProxySettings = true;
+                };
+              };
+            };
+      })
+    ];
 }
